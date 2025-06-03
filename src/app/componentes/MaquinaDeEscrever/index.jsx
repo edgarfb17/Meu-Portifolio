dd90a440940768c4ca23d123a3ed08e825954762
edgarFB <edgarfb@outlook.com.br>
2025-06-03 07:06:10 -0300

    manha

--- src/app/componentes/MaquinaDeEscrever/index.jsx
+++ src/app/componentes/MaquinaDeEscrever/index.jsx
@@ -6,9 +6,13 @@ export function MaquinaDeEscrever({txtRecebido}){
     
     const [ text, setText ] = useState("")
 
-    const escreverTexto = (txtRecebido) => {
-        let textoLetras 
-        //setTimeOut( ,200)
+    const escreverTexto = (txtRecebido, i = 0) => {
+        let textoLetras = txtRecebido.split("")
+        setTimeOut(setText(text + textoLetras[i]),200)
+        i++
+        if(text.length ==! txtRecebido.length){
+            escreverTexto(txtRecebido)
+        }
     }
     
     return(
