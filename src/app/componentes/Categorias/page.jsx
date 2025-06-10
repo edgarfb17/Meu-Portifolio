2ca2518a027e3c8164d188cbc87aed492dd0f17f
edgarFB <edgarfb@outlook.com.br>
2025-06-10 20:44:04 -0300

    casa responsivoPt3

--- src/app/componentes/Categorias/page.jsx
+++ src/app/componentes/Categorias/page.jsx
@@ -7,10 +7,10 @@ import React from "../../../../public/react.png"
 export default function Categorias({changeFilter, buttonClick}){
     return(
         <div className="alinhamento-conteudo" id={styles.container_categorias}>
-            <button onClick={() => changeFilter("todas")} className={buttonClick === "todas" ? styles.btnAcesso : styles.btnApagado}>Todas</button>
-            <button onClick={() => changeFilter("html/css")} className={buttonClick === "html/css" ? styles.btnAcesso : styles.btnApagado}><Image src={HtmlCss} alt="icon html/css"></Image>HTML/CSS</button>
-            <button onClick={() => changeFilter("js")} className={buttonClick === "js" ? styles.btnAcesso : styles.btnApagado}><Image src={Js} alt="icon js"></Image>JavaScript</button>
-            <button onClick={() => changeFilter("react")} className={buttonClick === "react" ? styles.btnAcesso : styles.btnApagado}><Image src={React} alt="icon react"></Image>React</button>
+            <button onClick={() => changeFilter("todas")} className={buttonClick === "todas" ? styles.btnAcesso : styles.btnApagado}><h3>Todas</h3></button>
+            <button onClick={() => changeFilter("html/css")} className={buttonClick === "html/css" ? styles.btnAcesso : styles.btnApagado}><Image src={HtmlCss} alt="icon html/css"></Image><h3>HTML/CSS</h3></button>
+            <button onClick={() => changeFilter("js")} className={buttonClick === "js" ? styles.btnAcesso : styles.btnApagado}><Image src={Js} alt="icon js"></Image><h3>JavaScript</h3></button>
+            <button onClick={() => changeFilter("react")} className={buttonClick === "react" ? styles.btnAcesso : styles.btnApagado}><Image src={React} alt="icon react"></Image><h3>React</h3></button>
         </div>
     )
 }
