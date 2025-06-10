92dfd7a0232446834d7a8bcf83e90cd4636309db
EdgarFB <edgarfb@outlook.com.br>
2025-06-10 13:54:22 -0300

    almoco responsivoPt3

--- src/app/componentes/Contato/page.jsx
+++ src/app/componentes/Contato/page.jsx
@@ -9,25 +9,25 @@ export default function Contato(props){
 
           <div id={styles.container_links}>
             <div className={styles.links}>
-              <Link href="https://wa.me/5515988231195">
+              <Link href="https://wa.me/5515988231195" target="_blank">
               <Image src={props.wpp} alt="icone whatsapp"></Image>
               (15)988231195
               </Link>
             </div>
             <div className={styles.links}>
-              <Link href="https://github.com/edgarfb17">
+              <Link href="https://github.com/edgarfb17" target="_blank">
               <Image src={props.git} alt="icone github"></Image>
               www.github.com/edgarfb17
               </Link>
             </div>
             <div className={styles.links}>
-              <Link href="https://www.linkedin.com/in/edgarfb/">
+              <Link href="https://www.linkedin.com/in/edgarfb/" target="_blank">
               <Image src={props.linkedin} alt="icone linkedin"></Image>
               www.linkedin.com/in/edgarfb/
               </Link>
             </div>
             <div className={styles.links}>
-              <Link href="mailto:edgarfb@outlook.com.br">
+              <Link href="mailto:edgarfb@outlook.com.br" target="_blank">
               <Image src={props.email} alt="icone email"></Image>
               edgarfb@outlook.com.br
               </Link>
