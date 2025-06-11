43c19e146ab8ebaa1ed51398db6954c92991df9a
EdgarFB <edgarfb@outlook.com.br>
2025-06-11 11:23:17 -0300

    commit responsivoPt4(cards)

--- src/app/componentes/Cards/page.jsx
+++ src/app/componentes/Cards/page.jsx
@@ -3,14 +3,16 @@ import Image from "next/image"
 
 export default function Cards(props){
     return(
-        <div id={styles.container_card}>
-            <h3>{props.titulo}</h3>
-            <div id={styles.container_imgs}>
-                <Image id={styles.gif_projeto} src={props.gif} alt="gifs projetos"></Image>
-                <Image id={styles.img_projeto} src={props.img} alt="img projeto"></Image>
-            </div>
-            <div id={styles.container_descricao}>
-                <p>{props.descricao}</p>
+        <div id={styles.container_position}>
+            <div id={styles.container_card}>
+                <h3>{props.titulo}</h3>
+                <div id={styles.container_imgs}>
+                    <Image id={styles.gif_projeto} src={props.gif} alt="gifs projetos"></Image>
+                    <Image id={styles.img_projeto} src={props.img} alt="img projeto"></Image>
+                </div>
+                <div id={styles.container_descricao}>
+                    <p>{props.descricao}</p>
+                </div>
             </div>
         </div>
     )
