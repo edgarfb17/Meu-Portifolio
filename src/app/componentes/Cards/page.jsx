3ad6dd6134407738b39b2632e76eb3428f672571
EdgarFB <edgarfb@outlook.com.br>
2025-06-18 11:36:49 -0300

    links projetos

--- src/app/componentes/Cards/page.jsx
+++ src/app/componentes/Cards/page.jsx
@@ -1,5 +1,6 @@
 import styles from "./cards.module.css"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Cards(props){
     return(
@@ -7,8 +8,9 @@ export default function Cards(props){
             <div id={styles.container_card}>
                 <h3>{props.titulo}</h3>
                 <div id={styles.container_imgs}>
-                    <Image id={styles.gif_projeto} src={props.gif} alt="gifs projetos"></Image>
+                    <Link href="#" id={styles.link_projeto}><Image id={styles.gif_projeto} src={props.gif} alt="gifs projetos"></Image></Link>
                     <Image id={styles.img_projeto} src={props.img} alt="img projeto"></Image>
+                    <span>Clique na imagem para ver o projeto</span>
                 </div>
                 <div id={styles.container_descricao}>
                     <p>{props.descricao}</p>
